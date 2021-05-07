5b52624f619272677b639dfcd0fdcb73bf8a2ef6
gokay <gokay@DESKTOP-NT8O59D>
2021-05-08 00:06:55 +0300

    Edit

--- class4Work/src/class4Work/Main.java
+++ class4Work/src/class4Work/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, RemoteException {
 		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MerniceServiceAdapter());
-		customerManager.save(new Customer(1,"Gökay","Yýldýrým",LocalDate.of(2000, 12, 22),"31534846048"));
+		customerManager.save(new Customer(1,"Gökay","Yýldýrým",LocalDate.of(2000, 8, 9),"31576510578"));
 		System.out.println();
 	}
 
